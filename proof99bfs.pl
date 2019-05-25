b7206fc07bf83fb1d8bbe3c31a47bda1e0ab104e
YvonneHoven <yvonne.hoven@hotmail.com>
2019-05-25 12:16:02 +0200

    Update proof99bfs.pl

--- proof99bfs.pl
+++ proof99bfs.pl
@@ -91,7 +91,7 @@ ass([not,not, '{', not,not,H, AO, H2, '}'|T], S):- wrt([not,not, '{', atm(not,at
 ass([not,not, '{', not,H, AO, not,not,H2, '}'|T], S):- wrt([not,not, '{', atm(not,H), AO, atm(not,atm(not,H2)), '}'], S), ass([atm(not,H), AO, atm(not,atm(not,H2))|T], S).
 ass([not,not, '{', not,H, AO, not,H2, '}'|T], S):- wrt([not,not, '{', atm(not,H), AO, atm(not,H2), '}'], S), ass([atm(not,H), AO, atm(not,H2)|T], S).
 ass([not,not, '{', not,H, AO, H2, '}'|T], S):- wrt([not,not, '{', atm(not,H), AO, H2, '}'], S), ass([atm(not,H), AO, H2|T], S).
-ass([not,not, '{', H, AO, not,not,H2, '}'|T], S):- wrt([not,not, '{', H, AO, atm(not,atm(not,H2), '}'], S), ass([H, AO, atm(not,atm(not,H2)|T], S).
+ass([not,not, '{', H, AO, not,not,H2, '}'|T], S):- wrt([not,not, '{', H, AO, atm(not,atm(not,H2)), '}'], S), ass([H, AO, atm(not,atm(not,H2))|T], S).
 ass([not,not, '{', H, AO, not,H2, '}'|T], S):- wrt([not,not, '{', H, AO, atm(not,H2), '}'], S), ass([H, AO, atm(not,H2)|T], S).
 %%assert notnot{}
 ass([not, '{', not,not,H, AO, not,not,H2, '}'|T], S):- ass([not, '{', atm(not,atm(not,H)), AO, atm(not,atm(not,H2)), '}'|T], S).
@@ -431,7 +431,7 @@ prove([not,not, '{', not,not,H, AO, H2, '}'|T], S):- prove([not,not, '{', atm(no
 prove([not,not, '{', not,H, AO, not,not,H2, '}'|T], S):- prove([not,not, '{', atm(not,H), AO, atm(not,atm(not,H2)), '}'|T], S).
 prove([not,not, '{', not,H, AO, not,H2, '}'|T], S):- prove([not,not, '{', atm(not,H), AO, atm(not,H2), '}'|T], S).
 prove([not,not, '{', not,H, AO, H2, '}'|T], S):- prove([not,not, '{', atm(not,H), AO, H2, '}'|T], S).
-prove([not,not, '{', H, AO, not,not,H2, '}'|T], S):- prove([not,not, '{', H, AO, atm(not,atm(not,H2), '}'|T], S).
+prove([not,not, '{', H, AO, not,not,H2, '}'|T], S):- prove([not,not, '{', H, AO, atm(not,atm(not,H2)), '}'|T], S).
 prove([not,not, '{', H, AO, not,H2, '}'|T], S):- prove([not,not, '{', H, AO, atm(not,H2), '}'|T], S).
 
 prove([not, '{', not,not,H, AO, not,not,H2, '}'|T], S):- prove([not, '{', atm(not,atm(not,H)), AO, atm(not,atm(not,H2)), '}'|T], S).
