08042a9de35533bb1869f2903fd442e963b138b9
YvonneHoven <yvonne.hoven@hotmail.com>
2019-05-26 17:58:22 +0200

    recursion solved

--- proof99dfs.pl
+++ proof99dfs.pl
@@ -273,7 +273,6 @@ prove([H, S1, not, H2, S2, not, H3|T], S3):- prove([H, S1, atm(not,H2), S2, atm(
 prove([H, S1, not, H2, S2, H3|T], S3):- prove([H, S1, atm(not,H2), S2, H3|T], S3).
 prove([H, S1, H2, S2, not,not, H3|T], S3):- prove([H, S1, H2, S2, atm(not,atm(not,H3))|T], S3).
 prove([H, S1, H2, S2, not, H3|T], S3):- prove([H, S1, H2, S2, atm(not,H3)|T], S3).
-prove([H, S1, H2, S2, H3|T], S3):- prove([H, S1, H2, S2, H3|T], S3).
 
 %%prove(A&B&C+)
 prove([H,'&',H2,'&',H3|T], '+'):- H3\='{', H\=not, H3\=not, H\=atm(not,_), H3\=atm(not,_), wrt([H,'&',H2,'&',H3], '+'), proof(H, '&', H2, '&', H3, '+'), prove(T, '+').
