20b53039482266ed857b26c676a2c2ce1e237643
YvonneHoven <yvonne.hoven@hotmail.com>
2019-05-27 10:25:03 +0200

    solve??

--- proof99dfs.pl
+++ proof99dfs.pl
@@ -80,6 +80,7 @@ printList([], _) :- nl.
 printList([atm(not,H)|T], S):- write("not "), write(H), write(", "), write(S),  write(' | '), printList(T, S).
 printList([H|T], S) :- H\=atm(not,_), write(H), write(", "), write(S), write(' | '), printList(T, S).
 
+prepareAnswer:- findall([Q3], noPrint(Q3, '+'), NP3), findall([Q4], noPrint(Q4, '-'), NP4), prepareAnswer1(NP1,NP2).
 prepareAnswer1([],[]):- nl, findall(Y, prf(Y, '+'), PL), findall(X, prf(X, '-'), NL), 
     	write('positive literals: '), nl, write("|"), printList(PL, '+'),
     	write('negative literals: '), nl, write("|"), printList(NL, '-').
@@ -96,15 +97,17 @@ wrt([atm(not,atm(not,A))|T],S):- A\=atm(not,_), write("notnot"), write(A), wrt(T
 wrt([atm(not,A)|T],S):- A\=atm(not,_), write("not"), write(A), wrt(T,S).
 wrt([H|T],S):- H\=atm(not,_), write(H), wrt(T,S).
 
+writeinf:- findall([Q1], noPrint(Q1,'+'), NP1), findall([Q2], noPrint(Q2,'-'), NP2), writeinf(NP1,NP2).
 writeinf([],[]):- writeln("inferences solving:").
 writeinf([_|_],[]):- fail.
 writeinf([],[_|_]):- fail. 
 writeinf([_|_],[_|_]):- fail. 
 
-prove(A, '|', C):- C\=[_], C\=[not,_], ass(A), wrt(C, '-'), findall([Z], assprove(Z, '+'), AS), findall([Q1], noPrint(Q1, '+'), NP1), findall([Q2], noPrint(Q2, '-'), NP2), check(NP1,NP2,AS), writeinf(NP1,NP2), prove(C, '-', NP1,NP2), findall([Q3], noPrint(Q3, '+'), NP3), findall([Q4], noPrint(Q4, '-'), NP4) , prepareAnswer1(NP3,NP4).
-prove(A, '|', C):- C=[B], ass(A), wrt(C, '-'), assert(prf(B, '-')), findall([Z], assprove(Z, '+'), AS), findall([Q1], noPrint(Q1,'+'), NP1), findall([Q2], noPrint(Q2,'-'), NP2), check(NP1,NP2,AS), findall([Q3], noPrint(Q3, '+'), NP3), findall([Q4], noPrint(Q4, '-'), NP4), prepareAnswer1(NP3,NP4).
-prove(A, '|', C):- C=[not,B], ass(A), wrt(C, '-'), assert(prf(atm(not,B), '-')), findall([Z], assprove(Z, '+'), AS), findall([Q1], noPrint(Q1,'+'), NP1), findall([Q2], noPrint(Q2,'-'), NP2), check(NP1,NP2,AS), findall([Q3], noPrint(Q3, '+'), NP3), findall([Q4], noPrint(Q4, '-'), NP4), prepareAnswer1(NP3,NP4).
+prove(A, '|', C):- C\=[_], C\=[not,_], ass(A), wrt(C, '-'), findall([Z], assprove(Z, '+'), AS), check(AS), writeinf, provee(C, '-'), prepareAnswer.
+prove(A, '|', C):- C=[B], ass(A), wrt(C, '-'), assert(prf(B, '-')), findall([Z], assprove(Z, '+'), AS), check(AS), prepareAnswer.
+prove(A, '|', C):- C=[not,B], ass(A), wrt(C, '-'), assert(prf(atm(not,B), '-')), findall([Z], assprove(Z, '+'), AS), check(AS), prepareAnswer.
 
+check([]):- findall([Q1], noPrint(Q1,'+'), NP1), findall([Q2], noPrint(Q2,'-'), NP2), check(NP1,NP2,[]). 
 check([],[],[]):- nl.
 check(NP1,NP2,_):- NP1\=[], NP2\=[], fail.
 check(NP1,[],_):- NP1\=[], fail.
@@ -113,6 +116,7 @@ check([],[],[[H]|T]):- nl, H\=[[]], writeln("premises solving:"), prsolve([[H]|T
 prsolve([]).
 prsolve([[H]|T]):- prove(H, '+'), prsolve(T).
 
+provee(C,'-'):- findall([Q1], noPrint(Q1,'+'), NP1), findall([Q2], noPrint(Q2,'-'), NP2), prove(C,'-',NP1,NP2). 
 prove(C,'-',[],[]):- prove(C, '-').
 prove(_,'-',[_|_],[]):- fail.
 prove(_,'-',[],[_|_]):- fail.
